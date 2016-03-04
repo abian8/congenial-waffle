23240fe565d38fe491b829b2186be88152017aff
Alex Bian <abian8@students.d125.org>
2016-03-04 13:49:01 -0600

    Albert is salty

---
+++ test/albertIsSoSalty.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class albertIsSoSalty here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class albertIsSoSalty
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Kyogre Kyogre Kyogre Kyogre Kyogre Kyogre Kyogre Kyogre Kyogre Kyogre");
+    }
+}
